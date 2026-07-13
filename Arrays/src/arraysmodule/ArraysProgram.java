d39606869263ce8da693c8f32a3a1af54d2d917d
abhinav885 <abhinav.s.nambiar@gmail.com>
2026-07-13 19:22:11 +0530

    Added Arrays folder

---
+++ Arrays/src/arraysmodule/ArraysProgram.java
@@ -0,0 +1,14 @@
+package arraysmodule;
+
+public class ArraysProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] ar = {1,2,3,4,5};
+		for (int i = 0; i < ar.length; i++) {
+			System.out.print(ar[i]+" ");
+		}
+
+	}
+
+}
