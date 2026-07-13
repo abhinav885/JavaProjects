75bb9631aa3a8638548e6f795ce33fee21446510
abhinav885 <abhinav.s.nambiar@gmail.com>
2026-07-13 19:23:46 +0530

    updated arraysprogram.java

--- Arrays/src/arraysmodule/ArraysProgram.java
+++ Arrays/src/arraysmodule/ArraysProgram.java
@@ -4,7 +4,7 @@ public class ArraysProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] ar = {1,2,3,4,5};
+		int[] ar = {1,2,3,4,5,6,7,8,9,10};
 		for (int i = 0; i < ar.length; i++) {
 			System.out.print(ar[i]+" ");
 		}
